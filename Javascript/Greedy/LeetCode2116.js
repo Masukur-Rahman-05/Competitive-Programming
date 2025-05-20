f8629196700fc986583fb8f587b04f10699245ff
Masuk <masukrahoman5@gmail.com>
2025-05-20 14:08:28 +0600

    Find if a string can be valid after changes

---
+++ Javascript/Greedy/LeetCode2116.js
@@ -0,0 +1,78 @@
+/*
+A parentheses string is a non-empty string consisting only of '(' and ')'. It is valid if any of the following conditions is true:
+
+    It is ().
+    It can be written as AB (A concatenated with B), where A and B are valid parentheses strings.
+    It can be written as (A), where A is a valid parentheses string.
+
+You are given a parentheses string s and a string locked, both of length n. locked is a binary string consisting only of '0's and '1's. For each index i of locked,
+
+    If locked[i] is '1', you cannot change s[i].
+    But if locked[i] is '0', you can change s[i] to either '(' or ')'.
+
+Return true if you can make s a valid parentheses string. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "))()))", locked = "010100"
+Output: true
+Explanation: locked[1] == '1' and locked[3] == '1', so we cannot change s[1] or s[3].
+We change s[0] and s[4] to '(' while leaving s[2] and s[5] unchanged to make s valid.
+
+Example 2:
+
+Input: s = "()()", locked = "0000"
+Output: true
+Explanation: We do not need to make any changes because s is already valid.
+
+Example 3:
+
+Input: s = ")", locked = "0"
+Output: false
+Explanation: locked permits us to change s[0]. 
+Changing s[0] to either '(' or ')' will not make s valid.
+
+Example 4:
+
+Input: s = "(((())(((())", locked = "111111010111"
+Output: true
+Explanation: locked permits us to change s[6] and s[8]. 
+We change s[6] and s[8] to ')' to make s valid.
+
+*/
+
+const canValid = (s, locked) => {
+  if (s.length % 2 !== 0) return false;
+
+  let maxOpen = 0;
+  let minOpen = 0;
+
+  let length = s.length;
+
+  for (let i = 0; i < length; i++) {
+    if (locked[i] === "1") {
+      if (s[i] === "(") {
+        maxOpen++;
+        minOpen++;
+      } else {
+        maxOpen--;
+        minOpen--;
+      }
+    } else {
+      maxOpen++;
+      minOpen--;
+    }
+
+    if (maxOpen < 0) return false;
+    if (minOpen < 0) minOpen = 0;
+  }
+
+  return minOpen === 0;
+};
+
+console.log(canValid("))()))", "010100"));
+console.log(canValid("()()", "0000"));
+console.log(canValid(")", "0"));
+console.log(canValid("(((())(((())", "111111010111"));
