e754233ddf0a6b99050162b16998babdb008629f
Masuk <masukrahoman5@gmail.com>
2025-06-10 09:57:31 +0600

    Find the target value in 2D matrix

---
+++ Javascript/Searching Algo/Binary Search/Problems/LeetCode240.js
@@ -0,0 +1,77 @@
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+    Integers in each row are sorted in ascending from left to right.
+    Integers in each column are sorted in ascending from top to bottom.
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Example 2:
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+
+*/
+
+const SearchIn2D = (matrix, target) => {
+  let row = 0;
+  let col = matrix[o].length - 1;
+
+  while (row < length && col >= 0) {
+    let n = matrix[row][col];
+
+    if (n === target) {
+      return true;
+    } else if (n > target) {
+      col--;
+    } else {
+      row++;
+    }
+  }
+
+  return false;
+};
+
+console.log(
+  SearchIn2D(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    5
+  )
+);
+
+console.log(
+  SearchIn2D(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    20
+  )
+);
+
+console.log(
+  SearchIn2D(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    1
+  )
+);
