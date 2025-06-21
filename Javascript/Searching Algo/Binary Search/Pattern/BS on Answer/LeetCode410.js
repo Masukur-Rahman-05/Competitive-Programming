31d3e69afdd2809b65b1d4b52b3ea84934fc53be
Masuk <masukrahoman5@gmail.com>
2025-06-21 09:41:19 +0600

    minimized the maximum sum in subArray

---
+++ Javascript/Searching Algo/Binary Search/Pattern/BS on Answer/LeetCode410.js
@@ -0,0 +1,63 @@
+/*
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the largest sum among the two subarrays is only 18.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+
+*/
+
+const minSubArraySum = (nums, k) => {
+  let left = Math.max(...nums);
+  let right = nums.reduce((acc, value) => acc + value, 0);
+
+  let len = nums.length;
+  let result = 0;
+
+  const canSplit = (maxSum) => {
+    let sum = 0;
+    let count = 1;
+
+    for (let i = 0; i < len; i++) {
+      if (sum + nums[i] > maxSum) {
+        count++;
+        sum = nums[i];
+      } else {
+        sum += nums[i];
+      }
+    }
+    return count <= k;
+  };
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (canSplit(mid)) {
+      result = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(minSubArraySum([7, 2, 5, 10, 8], 2));
+console.log(minSubArraySum([1, 2, 3, 4, 5], 2));
