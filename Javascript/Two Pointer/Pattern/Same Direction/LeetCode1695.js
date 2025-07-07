f02ed3884190628da2bd13c5458f8460a9a453ac
Masuk <masukrahoman5@gmail.com>
2025-07-07 09:08:12 +0600

    Find the max summation of subArray with unique values

---
+++ Javascript/Two Pointer/Pattern/Same Direction/LeetCode1695.js
@@ -0,0 +1,45 @@
+/*
+You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,4,5,6]
+Output: 17
+Explanation: The optimal subarray here is [2,4,5,6].
+
+Example 2:
+
+Input: nums = [5,2,1,2,5,2,1,2,5]
+Output: 8
+Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+
+*/
+
+const maxUniqueSum = (nums) => {
+  let max = 0;
+  let sum = 0;
+  let left = 0;
+
+  let set = new Set();
+
+  for (let right = 0; right < nums.length; right++) {
+    while (set.has(nums[right])) {
+      set.delete(nums[left]);
+      sum -= nums[left];
+      left++;
+    }
+    set.add(nums[right]);
+    sum += nums[right];
+    max = Math.max(max, sum);
+  }
+  return max;
+};
+
+console.log(maxUniqueSum([4, 2, 4, 5, 6]));
+console.log(maxUniqueSum([5, 2, 1, 2, 5, 2, 1, 2, 5]));
